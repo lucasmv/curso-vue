0870f58a6f967663890273afd6eaced995e09d3a
Lucas Magno <lucas.mv.silva@gmail.com>
2019-05-20 13:10:41 -0300

    commit componentes

--- componente-desafio/src/App.vue
+++ componente-desafio/src/App.vue
@@ -1,65 +1,15 @@
 <template>
-	<v-app>
-		<v-navigation-drawer app>
-			<v-toolbar flat>
-				<v-list>
-					<v-list-tile>
-						<v-list-tile-title class="title">
-							Menu
-						</v-list-tile-title>
-					</v-list-tile>
-				</v-list>
-			</v-toolbar>
+  <v-app>
+    <app-navigation />
 
-			<v-divider></v-divider>
+    <app-toolbar/>
 
-			<v-list dense class="pt-0">
-				<v-list-tile v-for="item in itensMenu" :key="item.titulo">
-					<v-list-tile-action>
-						<v-icon>{{ item.icon }}</v-icon>
-					</v-list-tile-action>
-					<v-list-tile-content>
-						<v-list-tile-title>{{ item.titulo }}</v-list-tile-title>
-					</v-list-tile-content>
-				</v-list-tile>
-			</v-list>
-		</v-navigation-drawer>
+    <app-carousel/>
 
-		<v-toolbar app>
-			<v-toolbar-title class="headline text-uppercase">
-				<span>Desafio</span>
-				<span class="font-weight-light">Componente</span>
-			</v-toolbar-title>
-		</v-toolbar>
-
-		<v-content>
-			<v-carousel>
-				<v-carousel-item
-					v-for="(item,i) in fotos"
-					:key="i"
-					:src="item.src"></v-carousel-item>
-			</v-carousel>
-		</v-content>
-
-		<app-footer />
-	</v-app>
+    <app-footer/>
+  </v-app>
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			itensMenu: [
-				{titulo: 'Início', icon: 'dashboard'},
-				{titulo: 'Sobre', icon: 'question_answer'}
-			],
-			fotos: [
-				{src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
-				{src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
-				{src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
-				{src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
-			]
-		}
-	}
-}
+export default {};
 </script>
--- componente-desafio/src/main.js
+++ componente-desafio/src/main.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 
+import Navigation from './Navigation'
+import Carousel from './Carousel'
+import Toolbar from './Toolbar'
 import Footer from './Footer'
 
 Vue.config.productionTip = false
 
+Vue.component('app-navigation', Navigation)
+Vue.component('app-carousel', Carousel)
+Vue.component('app-toolbar', Toolbar)
 Vue.component('app-footer', Footer)
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
