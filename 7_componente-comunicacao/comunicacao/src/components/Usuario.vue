827de8e76cea9119f64268f816d7596998663f8c
Lucas Magno <lucas.mv.silva@gmail.com>
2019-05-20 21:48:29 -0300

    commit

--- 7_componente-comunicacao/comunicacao/src/components/Usuario.vue
+++ 7_componente-comunicacao/comunicacao/src/components/Usuario.vue
@@ -1,52 +1,72 @@
 <template>
-    <div class="container">
-        <h1>Componente Usuário</h1>
-        <p>Esse é um componente muito legal!</p>
-        <p>Nome é: <strong>{{ nome }}</strong></p>
-        <button @click="alterarNome">Alterar Nome</button>
-        <hr>
-        <div class="componentes">
-            <app-usuario-info :nome="nome"/>
-            <app-usuario-editar />
-        </div>
+  <div class="container">
+    <h1>Componente Usuário</h1>
+    <p>Esse é um componente muito legal!</p>
+    <p>
+      Nome é:
+      <strong>{{ nome }}</strong>
+    </p>
+    <p>
+      Idade é:
+      <strong>{{ idade }}</strong>
+    </p>
+    <button @click="alterarNome">Alterar Nome</button>
+    <hr>
+    <div class="componentes">
+      <app-usuario-info
+        :nome="nome"
+        :idade="idade"
+        @nomeMudou="nome = $event"
+        :reiniciarFn="reiniciarNome"
+      />
+
+      <app-usuario-editar :idade="idade" @idadeMudou="idade = $event" :idadeBentoFn="idadeBento"/>
     </div>
+  </div>
 </template>
 
 <script>
-import AppUsuarioInfo from './UsuarioInfo'
-import AppUsuarioEditar from './UsuarioEditar'
+import AppUsuarioInfo from "./UsuarioInfo";
+import AppUsuarioEditar from "./UsuarioEditar";
 
 export default {
-    components: { AppUsuarioInfo, AppUsuarioEditar },
-    data(){
-        return {
-            nome: 'Lucas'
-        }
+  components: { AppUsuarioInfo, AppUsuarioEditar },
+  data() {
+    return {
+      nome: "Lucas",
+      idade: 29
+    };
+  },
+  methods: {
+    alterarNome() {
+      this.nome = "Bento";
     },
-    methods:{
-        alterarNome(){
-            this.nome = 'Bento'
-        }
+    reiniciarNome() {
+      this.nome = "Lucas";
+    },
+    idadeBento() {
+      this.idade = 1;
     }
-}
+  }
+};
 </script>
 
 <style scoped>
-    .container {
-        background-color: #27363b;
-        color: #fff;
-        padding: 10px;
-    }
+.container {
+  background-color: #27363b;
+  color: #fff;
+  padding: 10px;
+}
 
-    .container hr {
-        margin: 20px 10px;
-    }
+.container hr {
+  margin: 20px 10px;
+}
 
-    .componentes {
-        display: flex;
-    }
+.componentes {
+  display: flex;
+}
 
-    .componentes > * {
-        margin: 10px;
-    }
+.componentes > * {
+  margin: 10px;
+}
 </style>
--- 7_componente-comunicacao/comunicacao/src/components/UsuarioEditar.vue
+++ 7_componente-comunicacao/comunicacao/src/components/UsuarioEditar.vue
@@ -2,12 +2,20 @@
     <div class="componente">
         <h2>Alterar os Dados de Usuário</h2>
         <p>Edite as informações</p>
+        <p>Idade do Usuário <strong>{{ idade }}</strong> </p>
+        <button @click="alterarIdade">Alterar Idade</button>
+        <button @click="idadeBentoFn">Alterar Idade (Callback)</button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    props: ['idade', 'idadeBentoFn'],
+    methods:{
+        alterarIdade(){
+            this.$emit('idadeMudou', 30);
+        }
+    }
 }
 </script>
 
